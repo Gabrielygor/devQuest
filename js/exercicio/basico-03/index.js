a0df964e42226048caf22a1b945caaa4e7d05f3f
Gabrielygor <gabrielcanuto042@gmail.com>
2024-03-20 09:42:12 -0300

    Exercicio 03

---
+++ js/exercicio/basico-03/index.js
@@ -0,0 +1,10 @@
+
+
+let resposta = confirm("Deseja passa a noite??")
+
+
+if (resposta == false) { //Confere se ele clickou no nao
+    alert("Que pena")
+} else {
+    alert ("Seja bem vindo") //Confere se ele clickou no sim
+}
