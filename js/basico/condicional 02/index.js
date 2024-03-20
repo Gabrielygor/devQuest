73e8bd909c48297b023357fb676794e4cb6e3524
Gabrielygor <gabrielcanuto042@gmail.com>
2024-03-20 08:42:09 -0300

    Condicional 02

---
+++ js/basico/condicional 02/index.js
@@ -0,0 +1,15 @@
+
+
+
+let movieName = "vingadores"
+
+switch (movieName) {
+
+    case "vingadores" :
+        console.log("Vingadores");
+        break
+    case "Batman" :
+        console.log("Filme do batman");
+        break    
+
+}
