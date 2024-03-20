9a1f6055e6bc0116f92fe72fdc92f02834c6d9c1
Gabrielygor <gabrielcanuto042@gmail.com>
2024-03-20 08:17:45 -0300

    Laços de repetição

---
+++ js/basico/lacos/index.js
@@ -0,0 +1,20 @@
+
+//FOR para quando souber o numero a ser repitido
+
+//WHILE para quando nao souber
+
+let a = 1;
+
+
+if (a == 1) {
+    for (a = 1; a <= 10; a++) {
+        console.log("oi");
+    };
+
+} else {
+    while (a <= 10) {
+        console.log("Hello");
+        a++;
+    };
+};
+
