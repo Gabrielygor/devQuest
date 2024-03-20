64d6c03a9d949cb253a0011f3e862b720f6a4145
Gabrielygor <gabrielcanuto042@gmail.com>
2024-03-20 08:33:08 -0300

    Condicionais 01

---
+++ js/basico/condicional/index.js
@@ -0,0 +1,13 @@
+
+
+
+let movieName = "Supermen"
+
+
+if (movieName === "Batman") {
+    console.log("é o filme do Batman");
+} else if (movieName === "Supermen") {
+    console.log("é o filme do Supermen");
+} else {
+    console.log("é outro filme");
+}
