5dd6fd8deaf4e0f79c0b016c66742ea1afe99953
Gabrielygor <gabrielcanuto042@gmail.com>
2024-03-22 08:36:28 -0300

    Arrys

---
+++ js/intermediario/array/index.js
@@ -0,0 +1,20 @@
+// let filmes = ['Big Bang' , 'Vingadores' , 'Thor', '0']; 
+
+
+// console.log(filmes);
+
+
+let filmes = [
+    ['Vingadores', '0'],
+    ['Batman', '10', ['gal gadoa']],
+    ['Vingadores', '0'],
+    ['Vingadores', '0'],
+    ['Vingadores', '0'],
+    ['Vingadores', '0'],
+]
+
+for (let i = 0; i < filmes.length; i++) {
+    console.log("Filme:" + filmes[i] );
+}
+
+
