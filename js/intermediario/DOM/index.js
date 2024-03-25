1fc22eb87aa392b84c34710eade194965290070d
Gabrielygor <gabrielcanuto042@gmail.com>
2024-03-25 09:24:54 -0300

    Inicio do estudo com DOM (IMPORTANTE JS) é o freelas

---
+++ js/intermediario/DOM/index.js
@@ -0,0 +1,12 @@
+
+
+// Uma das merdas mais inmportantes do JS
+
+// console.log(document);
+
+// let p = document.getElementById('paragrafo1')
+
+let p = document.getElementsByClassName('oi')
+
+
+console.log(p);
