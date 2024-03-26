46637a2579bf938baf2867ab544cb3303874dff9
Gabrielygor <gabrielcanuto042@gmail.com>
2024-03-26 08:44:48 -0300

    Inicio Dom

---
+++ js/intermediario/DOM02/index.js
@@ -0,0 +1,13 @@
+
+
+function adicionarComentario() {
+
+    let novoComentario = document.getElementsByName('novo-comentario');
+
+    let textoDigitado = novoComentario[0].value;
+
+    let novosComentarios = document.getElementById ('novos-comentarios')
+
+    novosComentarios.innerHTML += `<p> Novo Comentario: ${textoDigitado} </p>`
+
+}
