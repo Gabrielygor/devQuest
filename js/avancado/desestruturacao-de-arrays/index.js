97fdf77e3a41213bb2e371546e6554f71b7fdfc7
Gabrielygor <gabrielcanuto042@gmail.com>
2024-04-10 08:50:47 -0300

    Desestruturando objetos

--- js/avancado/desestruturacao-de-arrays/index.js
+++ js/avancado/desestruturacao-de-arrays/index.js
@@ -1,7 +1,12 @@
-let pessoa = { nome: 'Gabriel', sobrenome: 'Canuto', idade: 20 };
+// let pessoa = { nome: 'Gabriel', sobrenome: 'Canuto', idade: 20 };
 
 
-let { nome: nome, idade: idade } = pessoa;
+// let { nome: nome, idade: idade } = pessoa;
 
 
-console.log(`Nome ${nome} e idade ${idade} `);
+// console.log(`Nome ${nome} e idade ${idade} `);
+
+
+const numeros = [1, 2, 3];
+
+const [um, dois, tres] = numeros;
