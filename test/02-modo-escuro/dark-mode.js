3468bbcb6e9b1eb6013409ab96298cbe8d87c466
Gabrielygor <gabrielcanuto042@gmail.com>
2024-04-12 07:56:08 -0300

    Modo escuro teste

--- test/02-modo-escuro/dark-mode.js
+++ test/02-modo-escuro/dark-mode.js
@@ -1,8 +1,16 @@
 const btnTema = document.querySelector(".botao-tema")
 const body = document.querySelector("body")
 
+const btnTemaImagemSol = document.querySelector(".sol")
+const btnTemaImagemLua = document.querySelector(".lua")
+
+
 
 btnTema.addEventListener("click", () => {
     body.classList.toggle("dark-theme")
-    console.log(body);
+    btnTemaImagemSol.classList.toggle("escondido")
+    btnTemaImagemLua.classList.toggle("aparecendoo")
 });
+
+//Ganbiara do krai mas funciona kkkkkkkkkkkkkkkkkkkk
+//kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
