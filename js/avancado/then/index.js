f3c54bbe056e9dbff3a43cb65a6523e41d513f69
Gabrielygor <gabrielcanuto042@gmail.com>
2024-04-22 10:00:51 -0300

    Then

---
+++ js/avancado/then/index.js
@@ -0,0 +1,27 @@
+//call back hell 
+
+
+let chaleiraEstaNoFogao = true
+let fogaoEstaLigado = true
+
+
+let ferverAgua = (chaleiraEstaNoFogao, fogaoEstaLigado) => {
+
+    return new Promise((resolve, reject) => {
+        if (chaleiraEstaNoFogao && fogaoEstaLigado) {
+            console.log("Fervendo agua");
+            resolve();
+        } else {
+            console.log("Nao fazendo cafe");
+            reject();
+        }
+    });
+};
+
+let passarCafe = () => {
+    console.log("Voce pode passar o cafe");
+}
+
+//Usando o then deixas as funçoes dependentes umas das outras
+
+ferverAgua(chaleiraEstaNoFogao, fogaoEstaLigado).then( passarCafe);
