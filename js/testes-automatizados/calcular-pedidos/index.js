79f24c8e18abc57d93442e242343c8b3a4a158ed
Gabrielygor <gabrielcanuto042@gmail.com>
2024-04-28 15:49:07 -0300

    teste

---
+++ js/testes-automatizados/calcular-pedidos/index.js
@@ -0,0 +1,27 @@
+
+
+const meuPedido = {
+    itens: [
+        { nome: 'Poção do Amor', valor: 100 },
+        { nome: 'Espada de Prata', valor: 50 },
+        { nome: 'Entrega', valor: 40, entrega: true }
+    ]
+};
+
+const calcularValorTotalDoPedido = pedido => {
+    const valorProdutos = pedido.itens
+        .filter(item => !item.entrega)
+        .reduce((totalPedidos, pedidoAtual) => totalPedidos + pedidoAtual.valor, 0); // Inicializando o total com zero
+
+    const valorEntrega = pedido.itens
+        .filter(item => item.entrega)
+        .reduce((totalEntrega, entregaAtual) => totalEntrega + entregaAtual.valor, 0); // Somando o valor de todas as entregas
+
+    if (valorProdutos > 500) {
+        return valorProdutos;
+    } else {
+        return valorProdutos + valorEntrega; // Adicionando o valor da entrega ao total, se necessário
+    }
+}
+
+console.log(calcularValorTotalDoPedido(meuPedido));
