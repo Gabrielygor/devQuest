60770405fd33d10f2dff90823412f3d56f67a4df
Gabrielygor <gabrielcanuto042@gmail.com>
2024-05-04 15:50:54 -0300

    Tentativa de correção

--- projeto-api-githUB/src/js/script.js
+++ projeto-api-githUB/src/js/script.js
@@ -18,12 +18,12 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
 
 
 async function user(userName) {
-    const response = await fetch(`https:api.github.com/users/${userName}`)
+    const response = await fetch(`https://api.github.com/users/${userName}`)
     return await response.json();
 };
 
 async function repos(userName) {
-    const response = await fetch(`https:api.github.com/users/${userName}/repos`)
+    const response = await fetch(`https://api.github.com/users/${userName}/repos`)
     return await response.json();
 }
 
