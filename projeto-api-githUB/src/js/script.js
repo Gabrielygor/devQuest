f9dc50ee7df7e9830b2270386a9ba1cd36639bc5
Gabrielygor <gabrielcanuto042@gmail.com>
2024-05-05 08:32:34 -0300

    alt script.js

--- projeto-api-githUB/src/js/script.js
+++ projeto-api-githUB/src/js/script.js
@@ -1,3 +1,9 @@
+import { user } from "./services/user.js"
+import { repos } from "./services/repos.js";
+
+
+
+
 document.getElementById('btn-search').addEventListener('click', () => {
 
     const userName = document.getElementById('input-search').value;
@@ -15,17 +21,6 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
     }
 
 });
-
-
-async function user(userName) {
-    const response = await fetch(`https://api.github.com/users/${userName}`)
-    return await response.json();
-};
-
-async function repos(userName) {
-    const response = await fetch(`https://api.github.com/users/${userName}/repos`)
-    return await response.json();
-}
 
 
 function getUserProfile(userName) {
