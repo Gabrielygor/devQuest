6df1991d78dbadd8f4419a6834673dd3a172e5cc
Gabrielygor <gabrielcanuto042@gmail.com>
2024-05-06 09:21:31 -0300

    Iniciando estudo usando REACT

---
+++ React/Baico/funcionamento-react/index.js
@@ -0,0 +1,8 @@
+const root = document.getElementById('root')
+const p = React.createElement('p' , null, 'Hello World!!! (Usando React)')
+const z = React.createElement('z', null , 'ASJDAKLSJDKLASDKASD')
+
+
+
+ReactDOM.render(p, root)
+ReactDOM.render(p,root)
